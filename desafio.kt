82b542c14230e3cc2d9fd3682c6f6c01b72129a3
Venilton FalvoJr <falvojr@gmail.com>
2022-10-17 23:06:14 -0300

    Template de código do desafio.

--- desafio.kt
+++ desafio.kt
@@ -1,1 +1,19 @@
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
+data class Usuario(val nome: String)
+
+data class ConteudoEducacional(val nome: String, val duracao: Int)
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Usuario>()
+    
+    fun matricular(usuario: Usuario) {
+        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    }
+}
+
+fun main() {
+    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+}
