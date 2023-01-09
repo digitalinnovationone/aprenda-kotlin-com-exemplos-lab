475d00cc65cc9928e14c31a5082f1ccedccc9938
lpraphael <raphael.perazzo@gmail.com>
2023-01-09 15:10:44 -0300

    realizado analise das classes e criação de teste de matricula de usuário

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,39 @@
+import java.util.Scanner
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+val scanner = Scanner(System.`in`)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(name: String, email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val dificuldade: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
+fun matricularAluno() {
+    println("Nome do aluno: ")
+    name = scanne.nextLine()
+    println("Email do aluno: ")
+    email = scanne.nextLine()
+    val user: Usuario = Usuario(name, email)
+    kotlinExperience.matricular(user)
+    println("O aluno $name foi matriculado com sucesso!")
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    val content: ConteudoEducacional = ("Conhecendo o Kotlin e Sua Documentação Oficial", 1, Nivel.BASICO)
+    val ContentList = arrayListOf<ConteudoEducacional>(content)
+    val user1: Usuario = Usuario("João", "joao@teste.com")
+    val user2: Usuario = Usuario("Maria", "maria@teste.com")
+    val kotlinExperience: Formacao = Formacao("Dominando a linguagem de programação Kotlin", contentList)
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    matricularAluno()
 }
