eef1bcffeaec95a4339a6540c41b2c1270c1bb47
João Pedro <jpedro1580@gmail.com>
2023-04-19 22:04:17 -0300

    Resolução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,64 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Level {
+  BASIC,
+  INTERMIDIATE,
+  ADVANCED
+}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+open class User(val name: String, val age: Int) {
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+  fun onNewContent(content: EducationalContent) {
+    content.let {
+      println(
+          "${name} diz: Legal! Agora posso aprender sobre ${it.name} em ${it.durationInMinutes} min"
+      )
     }
+  }
+}
+
+class Student(name: String, age: Int) : User(name, age)
+
+data class EducationalContent(val name: String, val durationInMinutes: Int = 60, val level: Level)
+
+// Classic pub-sub pattern
+data class Formation(val name: String, val contentList: MutableList<EducationalContent>) {
+  val subscribers = mutableListOf<User>()
+
+  fun subscribe(vararg users: User) {
+    users.forEach { user ->
+      user.let {
+        subscribers.add(it)
+        println("${it.name} se matriculou no curso $name")
+      }
+    }
+  }
+
+  fun addContent(content: EducationalContent) {
+    contentList.add(content)
+    notifyContentAdition(content)
+  }
+
+  fun notifyContentAdition(content: EducationalContent) {
+    for (subber in subscribers) {
+      subber.onNewContent(content)
+    }
+  }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+  val jose = Student("José", 19)
+  val james = Student("James", 27)
+  val jamile = Student("Jamile", 36)
+
+  val aula1 = EducationalContent("Componentes em React", 40, Level.BASIC)
+  val aula2 = EducationalContent("Estilização em React", 40, Level.BASIC)
+  val aula3 = EducationalContent("Estruturando componentes em React", 40, Level.BASIC)
+  val aula4 = EducationalContent("Props de componentes em React", 40, Level.INTERMIDIATE)
+
+  val curso1 = Formation("Iniciando em React", mutableListOf(aula1, aula2, aula3))
+
+  curso1.subscribe(jose)
+  curso1.subscribe(james, jamile)
+
+  curso1.addContent(aula4)
 }
