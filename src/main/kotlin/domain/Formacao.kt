6e12e6653ea51439ce5dc8bbfd40f24665d14906
Claudio Carige <ccarige@gmail.com>
2023-07-22 11:18:58 -0300

    Mudança na assinatura da classe com enum

--- src/main/kotlin/domain/Formacao.kt
+++ src/main/kotlin/domain/Formacao.kt
@@ -1,15 +1,12 @@
 package domain
 
+import domain.enums.Cursos
 import domain.enums.Nivel
 
-data class Formacao(val nome: String,var conteudos: MutableList<ConteudoEducacional>, val nivel: Nivel) {
+data class Formacao(val tipo: Cursos, var conteudos: MutableList<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
 
-
-    fun matricular(usuario: Usuario) {
-        inscritos.add(usuario)
-    }
     fun adicionarConteudo(conteudoEducacional: ConteudoEducacional){
         conteudos.add(conteudoEducacional)
     }
