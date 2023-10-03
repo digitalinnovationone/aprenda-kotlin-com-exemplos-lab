b21fb31926a4ff7981525a8eebd5dbb07217b299
tqi_eandre <tqi_eandre@uolinc.com>
2023-10-03 15:08:18 -0300

    implementando função matricular

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,32 @@
+import java.lang.IllegalArgumentException
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario (val nome: String, val idade: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if ( usuario.idade < 18 ){
+            throw IllegalArgumentException ("$nome não pode se matricular, pois a formação é apenas para maiores de idade")
+        }
+            inscritos.add(usuario)
+            println("Aluno ${usuario.nome} matriculado com sucesso na formação $nome (Conteúdo: ${conteudos.joinToString { it.nome }})")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val primeiroUsuario = Usuario("Eduardo", 32)
+    val segundoUsuario = Usuario("João", 15)
+    val primeiroConteudo = ConteudoEducacional("kotlin")
+    val segundoConteudo = ConteudoEducacional("Java")
+    val formacao = Formacao("TI", listOf(primeiroConteudo,segundoConteudo))
+    formacao.matricular(primeiroUsuario)
+    formacao.matricular(segundoUsuario)
 }
