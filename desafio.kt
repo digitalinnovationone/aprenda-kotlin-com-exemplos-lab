aad9f63bb904825ed1d87dfcea20ecec00f3c1ac
Marcelo Volpi <106558244+marcelo-volpi@users.noreply.github.com>
2023-10-09 20:02:39 +0200

    - Adicionado tempo para cada curso e de duração total - Adicionado teste para matriculas

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
+    fun calcularDuracaoTotal(): Int {
+        return conteudos.sumOf { it.duracao }
+    }
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", mutableListOf(conteudo1, conteudo2), Nivel.INTERMEDIARIO)
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    println("Formação: ${formacaoKotlin.nome}")
+    println("Nível: ${formacaoKotlin.nivel}")
+    println("Conteúdos:")
+    for (conteudo in formacaoKotlin.conteudos) {
+        println("- ${conteudo.nome}, Duração: ${conteudo.duracao} minutos")
+    }
+    println("Tempo total da formação: ${formacaoKotlin.calcularDuracaoTotal()} minutos")
+    println("Usuários matriculados na formação Kotlin: ${formacaoKotlin.inscritos.size}")
 }
