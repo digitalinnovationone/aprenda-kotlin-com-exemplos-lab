3ee24c029cde21a52ebbc5ff27762ede9fbfb953
Bruno Viera <125392891+BrunoMPVieira@users.noreply.github.com>
2023-10-18 13:13:43 -0300

    desafio.kt
    Desafio DIO concluído.

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,3 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
@@ -11,11 +9,22 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 90)
+
+    val formacao = Formacao("Desenvolvimento Android", listOf(conteudo1, conteudo2))
+
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos.size} usuários.")
 }
+
