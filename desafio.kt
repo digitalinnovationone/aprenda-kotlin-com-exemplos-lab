450e3fee7aa774425f7d8eeefbdb5412d49f76f9
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-12-10 03:37:33 -0300

    refactor

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,37 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class User (val name: String, val id: Int )
 
-class Usuario
+data class EducationContent (val name: String, val duration: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formation (val name: String, val level: String, var content: List<EducationContent>) {
+    val registered = mutableListOf<User>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+    fun register(user: User) {
+        registered.add(user)
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    }   
+
 }
 
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+   	//Criando instâncias de EducationContent
+    val content1 = EducationContent("POO", 60)
+    val content2 = EducationContent("Design System", 45)
+    val content3 = EducationContent("SGBD", 55)
+    
+    //Criando uma lista de EducationContent
+    val contentList = listOf(content1, content2, content3)
+    
+    //Criando uma instância de Formation
+    val formation = Formation("Backend", "Medio", contentList)
+    
+    //Criando um usuário
+    val user = User("Guilherme", 1)
+    
+    formation.register(user)
+    
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
