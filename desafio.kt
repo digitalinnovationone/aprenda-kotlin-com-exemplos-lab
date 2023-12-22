f3daeb40ac16ee9426c771f81d3c2558bc972281
nathanccoelho <nathancoelho.job@gmail.com>
2023-12-22 11:37:56 -0300

    Inserindo o nivel do conteudo educacional

--- desafio.kt
+++ desafio.kt
@@ -6,9 +6,9 @@ enum class Nivel {
 }
 class Usuario(var nome: String, var idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int ){
+data class ConteudoEducacional(var nome: String,val nivel: Nivel, val duracao: Int ){
     fun getInfo(): String{
-        return "O conteudo educacional escolhido é $nome e a duração é de $duracao dias!"
+        return "O conteudo educacional escolhido é $nome $nivel e tem duração de $duracao dias!"
     }
 }
 
@@ -23,8 +23,12 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 
 fun main() {
     val scan = Scanner(System.`in`)
-    val java = ConteudoEducacional(nome = "Java", duracao = 40)
-    val kotlin = ConteudoEducacional(nome = "Kotlin", duracao = 50)
+    val javaBasico = ConteudoEducacional(nome = "Java",nivel = Nivel.BASICO, duracao = 40)
+    val javaInter = ConteudoEducacional(nome = "Java",nivel = Nivel.INTERMEDIARIO, duracao = 50)
+    val javaDificil = ConteudoEducacional(nome = "Java",nivel = Nivel.DIFICIL, duracao = 60)
+    val kotlinBasico = ConteudoEducacional(nome = "Kotlin",nivel =Nivel.BASICO, duracao = 50)
+    val kotlinInter = ConteudoEducacional(nome = "Kotlin",nivel =Nivel.INTERMEDIARIO, duracao = 60)
+    val kotlinDificil = ConteudoEducacional(nome = "Kotlin",nivel =Nivel.DIFICIL, duracao = 70)
 
     println("Digite seu nome")
     val nome: String = scan.nextLine()
@@ -33,14 +37,27 @@ fun main() {
     var selecao: Boolean=true;
 
     while(selecao){
-        println("Olá, $nome\nDigite uma opção para iniciarmos\n1- Java\n2- Kotlin\n3- Sair")
+        println("Olá, $nome\nEscolha a linguagem de preferência\n1- Java\n2- Kotlin\n3- Sair")
         val opcao: Int = scan.nextInt()
         when(opcao){
             1 -> {
-                println(java.getInfo())
+                println("Escolha o nível \n1- Básico\n2- Intermediário \n3- Difícil")
+                val opcaoNivel: Int = scan.nextInt()
+                if(opcaoNivel == 1){
+                    println(javaBasico.getInfo())
+                } else if(opcaoNivel == 2){
+                    println(javaInter.getInfo())
+                } else if(opcaoNivel == 3){
+                    println(javaDificil.getInfo())
+                } else {
+                    println("Opção ínvalida")
+                }
                 
             }
-            2 -> println(kotlin.getInfo())
+            2 -> {
+                println("Escolha o nível \n1- Básico\n2- Intermediário \n3- Difícil")
+                val opcaoNivel: Int = scan.nextInt()
+            }
             3 -> {
                 println("Saindo do programa...")
                 selecao = false
