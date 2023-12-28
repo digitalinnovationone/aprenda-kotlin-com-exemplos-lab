15b014aa60f7ef2b956ed4365848af67f5c1d369
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-12-28 10:31:58 -0300

    initial commit: enroll user

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,16 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("$usuario matriculado na formação $nome")
+        } else {
+            println("O ${usuario.nome} já está matriculado na formação $nome!")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+//    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
