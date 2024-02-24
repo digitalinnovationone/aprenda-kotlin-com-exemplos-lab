922271fad655bd46c552d83bf434dfe6b486059d
EduardoHrq <eduardohrq.silva06@gmail.com>
2024-02-24 16:15:00 -0300

    finalizando desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
+import java.util.function.Predicate
+import java.util.stream.Collectors
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario (val name: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val inscritos: MutableList<Usuario> = mutableListOf()
+
+    fun matricular(vararg usuario: Usuario) {
+
+        for (user in usuario) {
+            inscritos.add(user)
+        }
+
+    }
+
+    override fun toString(): String {
+
+        val nomeDosConteudos = conteudos.map { c -> c.nome }.toList()
+
+        val estudantes = inscritos.map { u -> u.name }.toList()
+
+        return "Formacao: {\n" +
+                " Name: \"${nome}\",\n" +
+                " Conteudos: {\n" +
+                "   ${nomeDosConteudos}\n" +
+                " }\n" +
+                " Nivel: \"${nivel}\"\n" +
+                " Inscritos: {\n" +
+                "   ${estudantes}\n" +
+                " }"
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val formacaoKotlin = Formacao("Formacao em kotlin", listOf(ConteudoEducacional("Algoritmos"), ConteudoEducacional("POO Kotlin")), Nivel.BASICO)
+
+    formacaoKotlin.matricular(Usuario("Eduardo"), Usuario("Cathiely"))
+
+    println(formacaoKotlin.toString())
 }
