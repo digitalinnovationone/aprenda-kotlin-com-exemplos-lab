6c7077fce66184187491d8c8c0c9e89c897b640d
Ricardo Marino da Silva Oliveira <ricardomarino@live.com>
2024-03-14 12:15:17 -0300

    Implementação do desafio.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,82 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class UsuarioInvalidoException (message: String) : Exception(message)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Usuario (val nome: String, val email: String){
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    fun Validar() {
+        val builder = StringBuilder()
+        
+        if(nome.isEmpty()) builder.append("O nome do Usuário não foi informado. Verifique!\n") 
+        
+        if(email.isEmpty()) builder.append("O e-mail do Usuário não foi informado. Verifique!\n") 
+        
+        if(builder.length != 0) throw UsuarioInvalidoException(builder.toString())
+    }
+}
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
+
+data class Formacao(val nome: String, val nivel : Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        try {
+            
+            usuario.Validar()
+            inscritos.add(usuario)
+            println("=============================================")
+            println("O aluno ${usuario.nome} foi matriculado com o e-mail: ${usuario.email}")
+            println("=============================================\n")
+            
+        }   catch(e: UsuarioInvalidoException){
+            println("=============================================")
+            println("O usuário não foi vinculado")
+            println(e.message)
+            println("=============================================\n")
+        }
+    }
+    
+    fun gerarMassaAlunos(){
+        
+        for (i in 1..500) {
+    		inscritos.add(Usuario("Aluno ${i}", "email${i}@email.com"))
+		}
+    }
+    
+    fun listarAlunos(){
+        for(inscrito in inscritos){
+            println("=============================================")
+            println("O aluno ${inscrito.nome} foi matriculado com o e-mail: ${inscrito.email}")
+            println("=============================================\n")
+        }
+        	
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val listConteudoKotlin = mutableListOf<ConteudoEducacional>()
+    listConteudoKotlin.add(ConteudoEducacional("Primeiros Passos", 10))
+    listConteudoKotlin.add(ConteudoEducacional("Substituindo o Java", 100))
+    listConteudoKotlin.add(ConteudoEducacional("Web com Kotlin", 100))
+   
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.AVANCADO, listConteudoKotlin);
+
+    val usuarioValido =  Usuario("Ricardo", "ricardo@ricardo.com");
+    val usuarioInValidoNome =  Usuario("", "email@email.com");
+    val usuarioInValidoEmail =  Usuario("NOME", "");
+    val usuarioInvalido =  Usuario("", "");
+
+    formacaoKotlin.matricular(usuarioValido);
+    formacaoKotlin.matricular(usuarioInValidoNome);
+    formacaoKotlin.matricular(usuarioInValidoEmail);
+    formacaoKotlin.matricular(usuarioInvalido);
+    
+    formacaoKotlin.gerarMassaAlunos()
+    
+    formacaoKotlin.listarAlunos()
+       
 }
